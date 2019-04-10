c44a0723717300573eba3100b82c76e3b82c5db3
Moussa Boussi <mboussi@cumulus-erp.com>
2019-04-10 13:53:14 -0400

    profile pictures routes

--- src/index.js
+++ src/index.js
@@ -6,19 +6,16 @@ const userRouter = require('./routes/user')
 const app = express()
 const port = process.env.PORT || 3000
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => console.log(`Server is running at port ${port}`))
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById()
-    // await task.populate('owner').execPopulate()
-    
-    // const user = await User.findById()
-    // await user.populate('tasks').execPopulate()
-}
