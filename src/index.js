b9a6c7785c219b4cc5e4e4f6a2570630866f2636
Moussa Boussi <mboussi@cumulus-erp.com>
2019-04-10 14:00:26 -0400

    finalized image upload

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ const userRouter = require('./routes/user')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images'
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
